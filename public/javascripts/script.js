62440fd8befaac9772e51fe2f61a193b9af648a4
EdsonFragnan <edsonl.fragnan@gmail.com>
2016-11-22 18:48:52 -0200

    Update script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,12 @@
+
+var precoTotal = [];
+var precoCarrinho = [];
 function add(produto) {
   var contador = document.getElementById('carrinho').innerHTML;
-  var valContador = parseFloat(contador) + 1;
+  var valContador = parseInt(contador) + 1;
   document.getElementById('carrinho').innerText = valContador;
   var produtoCarrinho = document.getElementsByClassName(produto);
   var sacola = [];
-  var totalCarrinho;
   for(var i in produtoCarrinho) {
     var produto = {
       'idProduto': produtoCarrinho[0].textContent,
@@ -12,45 +14,54 @@ function add(produto) {
       'imagem': produtoCarrinho[1].currentSrc,
       'descricao': produtoCarrinho[2].textContent,
       'tamanho': produtoCarrinho[3].textContent,
-      'preco': produtoCarrinho[4].textContent,
-      'totalCarrinho': parseFloat(produtoCarrinho[4].textContent)
+      'preco': parseFloat(produtoCarrinho[4].textContent)
     };
   }
 
+  var cifrao = '<span class="cifrao">R$ </span>';
   sacola.push(
     '<div class="col-xs-6 col-lg-4 thumbnail thumb2"  id="'+ produto.sku +'">' +
         '<img src="'+ produto.imagem +'" class="imagem" alt="Corinthians" style="width:50%">' +
         '<p class="descProduto">' + produto.descricao + '</p>' +
         '<p class="tamanhoProduto">' + produto.tamanho + '</p>' +
-        '<p class="precProdutoCarrinho">R$ ' + produto.preco + '</p>' +
+        cifrao +
+        '<p class="precProdutoCarrinho">'+ produto.preco + '</p>' +
         '<input type="button" class="btn btn-default btn2" id="'+ produto.sku +'" onclick="remove(this.id);" value="Remover" />' +
     '</div>'
   );
 
-  var valProdutoCar = parseFloat(produto.preco);
-  var total = [];
+  precoTotal.push(parseFloat(produto.preco));
   var soma = 0;
-  total.push(valProdutoCar);
-  for (var i = 0; i < total.length; i++) {
-    soma = total[i] + soma;
+  for (var i = 0; i < precoTotal.length; i++) {
+   soma+= precoTotal[i];
   }
-
   sacola.toString();
   document.getElementById("prodCarrinho").insertAdjacentHTML('beforeend', sacola);
   document.getElementById("vazio").innerHTML = '';
-  document.getElementById("total").innerHTML = '<strong>TOTAL: R$ ' + soma + '0</strong>';
+  document.getElementById("cifrao3").innerText = 'TOTAL: R$ ';
+  document.getElementById("total").innerText = soma;
   document.getElementById("btnFecharCompra").style.display = "block";
 }
 
 function remove(id){
-  var produtoRemove = document.getElementById("prodCarrinho");
+  var valMenor = document.getElementById('total').innerHTML;
   var elemen = document.getElementById(id);
-  produtoRemove.removeChild(elemen);
+  var elemen2 = document.getElementById(id).childNodes;
+  var valElemen = parseFloat(elemen2[4].textContent);
+  var total = parseFloat(valMenor);
+  var subtrai = total - valElemen;
   var contador = document.getElementById('carrinho').innerHTML;
-  var valContador = parseFloat(contador) - 1;
+  var valContador = parseInt(contador) - 1;
   if (valContador === 0) {
+    document.getElementById("cifrao3").innerText = '';
     document.getElementById('vazio').innerHTML = '<strong>Carrinho vazio.</strong>';
-    document.getElementById('total').innerHTML = '<strong>TOTAL: R$ 0</strong>';
+    document.getElementById('total').innerHTML = '<strong>TOTAL: R$ '+ 0 +'</strong>';
+  } else {
+    document.getElementById("cifrao3").innerText = '';
+    document.getElementById('total').innerHTML = '<strong>TOTAL: R$ '+ subtrai +'</strong>';
   }
   document.getElementById('carrinho').innerText = valContador;
+  var produtoRemove = document.getElementById("prodCarrinho");
+  produtoRemove.removeChild(elemen);
+  precoTotal = [];
 }
