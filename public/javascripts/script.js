cd7ced0c6966e7808a157bee674d8ec4dddcfabc
Edson Luiz Fragnan <edsonl.fragnan@gmail.com>
2016-11-22 00:32:29 -0200

    Teste

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -18,22 +18,16 @@ function add(produto) {
     };
   }
 
-  var valCarrinho = produto.preco;
-  var campos = document.getElementsByClassName('precProduto');
-  console.log(campos);
-  //valCarrinho.push(produto.preco);
-
-  console.log(valCarrinho);
-
   sacola.push(
     '<div class="col-xs-6 col-lg-4 thumbnail thumb2"  id="'+ produto.sku +'">' +
         '<img src="'+ produto.imagem +'" class="imagem" alt="Corinthians" style="width:50%">' +
         '<p class="descProduto">' + produto.descricao + '</p>' +
         '<p class="tamanhoProduto">' + produto.tamanho + '</p>' +
-        '<p class="precProduto">R$ ' + produto.preco + '</p>' +
+        '<p class="precProdutoCarrinho">' + produto.preco + '</p>' +
         '<input type="button" class="btn btn-default btn2" id="'+ produto.sku +'" onclick="remove(this.id);" value="Remover" />' +
     '</div>'
   );
+
   sacola.toString();
   document.getElementById("prodCarrinho").insertAdjacentHTML('beforeend', sacola);
   document.getElementById("btnFecharCompra").style.display = "block";
